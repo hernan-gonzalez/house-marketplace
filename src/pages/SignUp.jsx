97c0e797e681d34749d3a22e5022a2fe259b7558
hernangonzalez1 <hernangonzalez1@outlook.com>
2022-08-10 21:39:49 -0400

    Added signup functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase.config';
 import { ReactComponent as ArrowRigthIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -21,6 +23,26 @@ export default function SignUp() {
         }))
     };
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const auth = getAuth();
+
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+            const user = userCredential.user;
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            });
+
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="pageContainer">
@@ -28,7 +50,7 @@ export default function SignUp() {
                     <p className="pageHeader">Welcome Back!</p>
                 </header>
                 <main>
-                    <form >
+                    <form onSubmit={onSubmit}>
                         <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange} />
                         <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
                         <div className="passwordInputDiv">
