249ae26f278cf19920a12304afa33a7dae1da735
Hernan Gonzalez <hegonzalez@transperfect.com>
2022-08-16 11:50:12 -0400

    Added list to profile and ability to delete

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { updateDoc, doc } from "firebase/firestore";
+import {
+    updateDoc,
+    doc,
+    collection,
+    getDocs,
+    query,
+    where,
+    orderBy,
+    deleteDoc,
+    getDoc,
+} from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
 import homeIcon from "../assets/svg/homeIcon.svg";
 import { Link } from "react-router-dom";
-
+import ListingItem from "../components/ListingItem";
 export default function Profile() {
+    const [listings, setListings] = useState(null);
+    const [loading, setLoading] = useState(true);
     const auth = getAuth();
     const [changeDetails, setChangeDetails] = useState(false);
     const [formData, setFormData] = useState({
@@ -19,6 +31,33 @@ export default function Profile() {
     const { name, email } = formData;
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchUserListings = async () => {
+            const listingsRef = collection(db, "listings");
+
+            const q = query(
+                listingsRef,
+                where("userRef", "==", auth.currentUser.uid),
+                orderBy("timestamp", "desc")
+            );
+
+            const querySnapshot = await getDocs(q);
+
+            let listings = [];
+
+            querySnapshot.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data(),
+                });
+            });
+            setListings(listings);
+            setLoading(false);
+        };
+        fetchUserListings();
+    }, [auth.currentUser.uid]);
+
     const onLogout = () => {
         auth.signOut();
         navigate("/");
@@ -47,6 +86,18 @@ export default function Profile() {
             [e.target.id]: e.target.value,
         }));
     };
+
+    const onDelete = async (listingId) => {
+        if (window.confirm("Are you sure you want to delete?")) {
+            await deleteDoc(doc(db, "listings", listingId));
+            const updatedListings = listings.filter(
+                (listing) => listing.id !== listingId
+            );
+            setListings(updatedListings);
+            toast.success("Successfully deleted listing");
+        }
+    };
+
     return (
         <div className="profile">
             <header className="profileHeader">
@@ -57,7 +108,7 @@ export default function Profile() {
             </header>
             <main>
                 <div className="profileDetailsHeader">
-                    <p className="profileDetailsText">Personal Details</p>
+                    <p className="personalDetailsText">Personal Details</p>
                     <p
                         className="changePersonalDetails"
                         onClick={() => {
@@ -101,6 +152,22 @@ export default function Profile() {
                     <p>Sell or rent your home</p>
                     <img src={arrowRight} alt="arrow right" />
                 </Link>
+
+                {!loading && listings?.length > 0 && (
+                    <>
+                        <p className="listingText">Your Listings</p>
+                        <ul className="listingsList">
+                            {listings.map((listing) => (
+                                <ListingItem
+                                    key={listing.id}
+                                    listing={listing.data}
+                                    id={listing.id}
+                                    onDelete={() => onDelete(listing.id)}
+                                />
+                            ))}
+                        </ul>
+                    </>
+                )}
             </main>
         </div>
     );
